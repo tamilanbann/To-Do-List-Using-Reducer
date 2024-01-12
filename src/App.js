c5a6ada9319e12742723c389b8e1f4a9f4ec0fb6
Tamilanban Nakkeeran <tamilanbannakkeeran@Tamilanbans-MacBook-Air.local>
2024-01-12 22:03:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ToDOList from "./ToDoList";
+
+function App() {
+  return (
+    <div className="App   bg-blue-300 min-h-screen ">
+      <ToDOList />
+    </div>
+  );
+}
+
+export default App;
